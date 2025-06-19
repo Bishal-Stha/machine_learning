30ca85efe9b7c3c1c6212876bf03a71ffac5fc6d
Bishal-Stha <programmerbishal@gmail.com>
2025-06-19 21:50:10 +0545

    Skin_cancer classification

---
+++ Real_ML_Stuff/PyTorch/imports.py
@@ -0,0 +1,12 @@
+# PyTorch core
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+
+# TorchVision for images
+from torchvision import datasets, transforms, models
+
+# Other helpful tools
+import matplotlib.pyplot as plt
+from PIL import Image
+from tqdm.auto import tqdm
